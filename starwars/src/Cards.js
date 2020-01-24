ae42041bed6d5352df4e7643987e6a87cb7da692
Alex Miller <alexmillerprojects@gmail.com>
2020-01-24 08:46:56 -0800

    api working all data present, working on styling

--- starwars/src/Cards.js
+++ starwars/src/Cards.js
@@ -1,28 +1,55 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const CharCard = props => {
 
-    
+    const Container = styled.div`
+        width: 90%;
+        margin: 0 auto;
+    `;
+
+    const StyledList = styled.li`
+        list-style-type: none;
+        padding: 1%;
+        margin: 10px auto;
+        background: lightgray;
+        border: 1px solid grey;
+        width: 75%;
+    `;
+
+    const CardHolder = styled.div`
+        width: 50%;
+    `;
+
+    const Listed = styled.ul`
+        margin: 0;
+        padding: 0;
+        width: 100%;
+        display:flex;
+        flex-direction: row;
+        justify-content: space-evenly;
+        flex-wrap: wrap;
+    `;
 
     return (
-        <div>
-            <ul>
+        <Container>
+            <Listed>
             {props.names.map(e => {
                 return (
-                    <div>
-                    <li key={props.names}>Name: {e.name}
+                    <CardHolder>
+                    <StyledList key={props.names}><strong>Name: </strong>{e.name}
                     <br/>
                     <br/>
                     <strong>Born: </strong> {e.birth_year}
                     <br/>
                     <br/>
                     <strong>Numbers of films: {e.films.length}</strong>
-                    </li>
-                    </div>
+                    </StyledList>
+                    </CardHolder>
                 )
             })}
-            </ul>
-        </div>
+            </Listed>
+        </Container>
     )
 }
 
